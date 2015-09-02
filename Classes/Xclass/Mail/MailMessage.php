e17eb0d3cabe6dacea07e98feb9e60f5a47669b2
Ameos <typo3dev@ameos.com>
2015-09-02 16:21:45 +0200

    [TASK] Respect PSR-1 and PSR-2 standard
    http://www.php-fig.org/psr/psr-1/
    http://www.php-fig.org/psr/psr-2/

--- Classes/Xclass/Mail/MailMessage.php
+++ Classes/Xclass/Mail/MailMessage.php
@@ -1,32 +1,46 @@
 <?php
 namespace Ameos\AmeosMailredirect\Xclass\Mail;
 
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+ 
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
-class MailMessage extends \TYPO3\CMS\Core\Mail\MailMessage {
+class MailMessage extends \TYPO3\CMS\Core\Mail\MailMessage 
+{
 
-	/**
-	 * @var array original recipient
-	 */ 
-	protected $originalRecipient;
+    /**
+     * @var array original recipient
+     */ 
+    protected $originalRecipient;
 
-	/**
+    /**
      * Get the To addresses of this message.
      *
      * @return array
      */
     public function getTo()
     {
-		$this->originalRecipient = parent::getTo();
-		$addresses = array();
-		$recipients = GeneralUtility::trimExplode(';', $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ameos_mailredirect']['recipient']);
-		foreach($recipients as $recipient) {
-			$addresses[$recipient] = '';
-		}
-		if (!$this->_setHeaderFieldModel('To', $addresses)) {
-            $this->getHeaders()->addMailboxHeader('To', $addresses);
-        }
-        return $addresses;
+	$this->originalRecipient = parent::getTo();
+	$addresses = array();
+	$recipients = GeneralUtility::trimExplode(';', $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ameos_mailredirect']['recipient']);
+	foreach ($recipients as $recipient) {
+	    $addresses[$recipient] = '';
+	}
+	if (!$this->_setHeaderFieldModel('To', $addresses)) {
+	    $this->getHeaders()->addMailboxHeader('To', $addresses);
+	}
+	return $addresses;
     }
 
     /**
@@ -36,13 +50,12 @@ class MailMessage extends \TYPO3\CMS\Core\Mail\MailMessage {
      *
      * @return string|null
      */
-    public function getBody() {
-		$body = parent::getBody();
-		$body.= '<br /><hr /><br />This mail must be sent to : ' . implode(';', array_keys($this->originalRecipient));
-		return $body;
-	}
-
-    
+    public function getBody() 
+    {
+	$body = parent::getBody();
+	$body.= '<br /><hr /><br />This mail must be sent to : ' . implode(';', array_keys($this->originalRecipient));
+	return $body;
+    }
 
     /**
      * Set the subject of this message.
@@ -51,12 +64,13 @@ class MailMessage extends \TYPO3\CMS\Core\Mail\MailMessage {
      *
      * @return Ameos\AmeosMailredirect\Xclass\Mail\MailMessage
      */
-    public function setSubject($subject) {
-		$prefix = $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ameos_mailredirect']['subject_prefix'];
-		if(trim($prefix) !== '') {
-			$subject = trim($prefix) . ' ' . $subject;
-		}
-		parent::setSubject($subject);
-		return $this;
+    public function setSubject($subject) 
+    {
+	$prefix = $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ameos_mailredirect']['subject_prefix'];
+	if (trim($prefix) !== '') {
+	    $subject = trim($prefix) . ' ' . $subject;
 	}
+	parent::setSubject($subject);
+	return $this;
+    }
 }
