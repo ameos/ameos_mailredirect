deb0d16cead929da6d7a419b40a05b0e44c0fe8f
Ameos <typo3dev@ameos.com>
2016-12-05 15:26:30 +0100

    [TASK] update constraint for allow php7.x

--- ext_emconf.php
+++ ext_emconf.php
@@ -19,10 +19,10 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType'         => '',
 	'author_company'   => '',
-	'version'          => '1.1.1',
+	'version'          => '1.1.2',
 	'constraints'      => array(
 		'depends'      => array (
-			'php' => '5.3.3-5.6.99',
+			'php' => '5.3.3-7.1.99',
 			'typo3' => '6.2.0-7.99.99',
 		),
 		'conflicts' => array(),
